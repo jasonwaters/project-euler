2994dd64a6f5d12c6f06c11eaea6eb8a067d21c7
Jason Waters <jasonwaters@users.noreply.github.com>
2017-07-31 13:47:32 -0700

    test coverage demo (#2)

--- src/fibonacci-even/fibonacci-even.spec.js
+++ src/fibonacci-even/fibonacci-even.spec.js
@@ -1,3 +1,13 @@
 const expect = require('chai').expect;
 
 const fibonacciEven = require('./fibonacci-even');
+
+describe('fibonacci-even', () => {
+
+  describe('determines nth number in fobonacci sequence', () => {
+    it('19th == 6765', () => {
+      let result = fibonacciEven.fib(19);
+      expect(result).to.equal(6765);
+    });
+  });
+});
